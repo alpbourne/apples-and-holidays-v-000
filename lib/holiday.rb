99551bd90a553aa3c6481b54c2a8909392b27ffd
alpbourne <alpbourne@gmail.com>
2017-09-22 21:34:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holidays, supplies|
       holiday_array = holidays.to_s.split("_")
       holiday_array.each {|w| w.capitalize!}
-      puts "(holiday_array.join(" ")): #{supplies.join(", ")}"
+      holiday_array.join(" ")
+      puts "#{holiday_array}: #{supplies.join(", ")}"
     end
   end
 end
